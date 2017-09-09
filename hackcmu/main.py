7536d544bf271a7f5e6a34d6d618438fafe3185b
Kyra Balenzano <kyra@Kyras-Air.wv.cc.cmu.edu>
2017-09-08 22:44:34 -0400

    Updated in main.py

--- hackcmu/main.py
+++ hackcmu/main.py
@@ -15,11 +15,28 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        template = jinja_environment.get_template('templates/hackcmu.html')
+        self.response.out.write(template.render())
         self.response.write('Hello world!')
 
+class WelcomeHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('templates/welcome.html')
+        self.response.out.write(template.render())
+        my_vars = {"name": "User"}
+        template = jinja_environment.get_template('templates/welcome.html')
+        self.response.out.write(template.render(my_vars))
+
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/welcome', WelcomeHandler)
 ], debug=True)
