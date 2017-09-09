d9e45e45915ee4f59918353f62d5a4bae214985c
Komal Dhull <komal@komal.wv.cc.cmu.edu>
2017-09-09 16:23:45 -0400

    :D

--- carbongarden/scripts/questions.js
+++ carbongarden/scripts/questions.js
@@ -62,4 +62,4 @@ document.getElementById("score").innerHTML= "Score: "  + score.toString();}
 
 
 
-Read more: http://javarevisited.blogspot.com/2013/02/disable-submit-button-in-html-javascript-avoid-multiple-form-submission.html#ixzz4sCdnfZ3s
+//Read more: http://javarevisited.blogspot.com/2013/02/disable-submit-button-in-html-javascript-avoid-multiple-form-submission.html#ixzz4sCdnfZ3s
