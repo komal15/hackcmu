0ab9207fd86768a44213a0a6bb14378095c04a21
Jeanette Duran <jeanetteduran@Jeanettes-MBP.wv.cc.cmu.edu>
2017-09-09 14:24:48 -0400

    questions

--- carbongarden/scripts/questions.js
+++ carbongarden/scripts/questions.js
@@ -56,8 +56,3 @@ document.getElementById("submit").onclick=function() {
 var score= check();
 console.log(score);
 document.getElementById("score").innerHTML= "Score: "  + score.toString();}
-
-
-
-
-Read more: http://javarevisited.blogspot.com/2013/02/disable-submit-button-in-html-javascript-avoid-multiple-form-submission.html#ixzz4sCdnfZ3s
