c82c5a1c20a2c76f87cd1878d2b7ce5baf60ad06
Kayla Vokt <kaylavokt@Kaylas-MBP.wv.cc.cmu.edu>
2017-09-09 17:18:06 -0400

    pretty q button

--- carbongarden/scripts/questions.js
+++ carbongarden/scripts/questions.js
@@ -28,7 +28,7 @@ function check() {
     points+=1;
   }
 
-  if( document.getElementById("Recycled all bottles and cans").check == true){
+  if( document.getElementById("Recycled all bottles and cans").checked == true){
     points+=1;
   }
 
@@ -57,8 +57,3 @@ document.getElementById("submit").onclick=function() {
 var score= check();
 console.log(score);
 document.getElementById("score").innerHTML= "Score: "  + score.toString();}
-
-
-
-
-Read more: http://javarevisited.blogspot.com/2013/02/disable-submit-button-in-html-javascript-avoid-multiple-form-submission.html#ixzz4sCdnfZ3s
